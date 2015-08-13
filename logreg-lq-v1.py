62472c54266cc8d0359b0613c6f302d464653134
usartkom <artem.komarichev@wayne.edu>
2015-08-13 12:00:35 -0400

    refactoring + prediction for 10 digits. got accuracy 95.2%

--- logreg-lq-v1.py
+++ logreg-lq-v1.py
@@ -20,8 +20,7 @@ data = scipy.io.loadmat('ex3data.mat')
 X = data['X']
 y = data['y']
 
-print type(X), X.shape
-print type(y), y.shape
+m, n = X.shape
 
 _lambda = 0.1
 
@@ -36,22 +35,7 @@ def f(x, *args):
 
     x = np.expand_dims(x, axis=1)
 
-    # print x.shape
-    # print X.shape
-    # print x.shape
-    # print "OK!!!"
     h = sigmoid(np.dot(X, x))
-    # print "OK!!!"
-    # print type(x)
-
-    #print -b.T * np.log(h) - (1 - b.T) * np.log(1 - h)
-    # print z.shape
-    # print type(x)
-    # print u.shape
-    # print(x - z.T + u.T).shape
-
-    # print(1.0 / m) * np.sum(-b * np.log(h) - (1 - b) * np.log(1 - h)) + \
-    #     (_lambda / (2 * m)) * np.linalg.norm(x - z + u, 2) ** 2
 
     return (1.0 / m) * np.sum(-b * np.log(h) - (1 - b) * np.log(1 - h)) + \
         (_lambda / (2 * m)) * np.linalg.norm(x - z + u, 2) ** 2
@@ -61,33 +45,12 @@ def gF(x, *args):
 
     X, b, z, u, rho, _lambda = args
     x = np.expand_dims(x, axis=1)
-    # print 'ok!'
+
     h = sigmoid(np.dot(X, x))
 
     m = X.shape[0]
 
-# print X.T.shape
-# print(h - b.T).T.shape
-
-    #print(np.dot(X.T, (h - b.T).T)).shape
-    # print np.asarray(x).shape
-    # print z.shape
-    # print u.shape
-    # print(x - z + u).shape
-
-    # print type(h - b)
-    # print(h - b).shape
-    # print X.T.shape
-    # print type(X.T)
-    #print(x - z + u).shape
-
     out = (1.0 / m) * (np.dot(X.T, (h - b)) + _lambda * (x - z + u))
-    # print 'OK!'
-    # print "gF:"
-    # print x.shape
-    # print type(x)
-    # print out.shape
-    # print type(out)
 
     return np.asarray(out, order='F')
 
@@ -97,10 +60,11 @@ def shrinkage(a, kappa):
     return np.maximum(0, (a - kappa)) - np.maximum(0, (-a - kappa))
 
 
-# def objective(A, b, mu, x, z):
-#     m = A.shape[0]
-# return np.sum(np.log(1 + np.exp(-A * x[1:] - b * x[0]))) + m * mu *
-# np.linalg.norm(z, 1)
+def objective(X, b, x, z, u, _lambda):
+    m = X.shape[0]
+    h = sigmoid(np.dot(X, x))
+    return (1.0 / m) * np.sum(-b * np.log(h) - (1 - b) * np.log(1 - h)) + \
+        (_lambda / (2 * m)) * np.linalg.norm(z, 2) ** 2
 
 
 def logreg(A, b, _lambda, rho, alpha):
@@ -115,37 +79,30 @@ def logreg(A, b, _lambda, rho, alpha):
     u = np.zeros((n + 1, 1))
 
     X = np.hstack((np.ones((m, 1)), A))
-    # C = np.concatenate((b, A), axis=0)
 
-    print '{0}\t{1}\t\t{2}\t\t{3}\t\t{4}\t{5}'.format('iter', 'r_norm', 'eps_pri', 's_norm', 'eps_dual', 'obj')
+    # print '{0}\t{1}\t\t{2}\t\t{3}\t\t{4}\t{5}'.format('iter', 'r_norm',
+    #                                                   'eps_pri', 's_norm', 'eps_dual', 'obj')
 
     for k in range(MAX_ITER):
         # x-update
 
         #print(x - z + u).shape
-        res = fmin_l_bfgs_b(f,
-                            np.zeros((n + 1, 1)),
-                            fprime=gF,
-                            # jac=gF,
-                            args=(X, b, z, u, rho, _lambda),
-                            # approx_grad=True,
-                            # epsilon=0.5
-                            # method='L-BFGS-B',
-                            # method='CG',
-                            # jac = gF,
-                            maxiter=25
-                            #options={'maxiter': 5, 'maxfun': 5}
-                            )
+        res = minimize(f,
+                       x,
+                       # fprime=gF,
+                       jac=gF,
+                       args=(X, b, z, u, rho, _lambda),
+                       # approx_grad=True,
+                       # epsilon=0.5
+                       method='L-BFGS-B',
+                       # method='CG',
+                       # jac = gF,
+                       # maxiter=25
+                       options={'maxiter': 25}
+                       )
         # print res
-        x = np.expand_dims(res[0], axis=1)
-        #x = np.expand_dims(res.x, axis=1)
-        # print 'ok'
-        # print res
-        # print res.shape
-        # x = res[0]
-        # print x.shape
-        obj = 0
-        # x = update_x(A, b, u, z, rho)
+        #x = np.expand_dims(res[0], axis=1)
+        x = np.expand_dims(res.x, axis=1)
 
         # z-update
         zold = np.copy(z)
@@ -156,10 +113,7 @@ def logreg(A, b, _lambda, rho, alpha):
         # u-update
         u = u + (x_hat - z)
 
-        # print 'z:', z.T
-        # print 'zold:', zold.T
-
-        #obj = objective(A, b, mu, x, z)
+        obj = objective(X, b, x, z, u, _lambda)
         r_norm = np.linalg.norm(x - z, 2)
         s_norm = np.linalg.norm(rho * (z - zold), 2)
 
@@ -167,15 +121,45 @@ def logreg(A, b, _lambda, rho, alpha):
             max(np.linalg.norm(x, 2), np.linalg.norm(z, 2))
         eps_dual = np.sqrt(n) * ABSTOL + RELTOL * np.linalg.norm(rho * u, 2)
 
-        print '{0}\t{1}\t{2}\t{3}\t{4}\t{5}'.format(k, r_norm, eps_pri,
-                                                    s_norm, eps_dual, obj)
+        # print '{0}\t{1}\t{2}\t{3}\t{4}\t{5}'.format(k, r_norm, eps_pri,
+        #                                             s_norm, eps_dual, obj)
 
         if r_norm < eps_pri and s_norm < eps_dual:
             break
 
-random_indecies = np.random.randint(0, y.shape[0], 5000)
+    return x
 
-#logreg(X, (y == 1) + 0, _lambda, 1.0, 1.0)
+# random_indecies = np.random.randint(0, y.shape[0], 5000)
 
-logreg(
-    X[random_indecies], (y[random_indecies] == 1) + 0, _lambda, 1.0, 1.0)
+
+all_theta = np.zeros((num_labels, n + 1))
+
+for c in range(0, num_labels):
+    if c == 0:
+        theta = logreg(X, (y == 10) + 0, _lambda, 1.0, 1.0)
+    else:
+        theta = logreg(X, (y == c) + 0, _lambda, 1.0, 1.0)
+
+    all_theta[c, :] = theta.T
+
+    print 'c: ', c
+
+X = np.hstack((np.ones((m, 1)), X))
+predictions = np.dot(all_theta, X.T)
+
+
+maxval = np.amax(predictions, axis=0)
+maxindices = np.argmax(predictions, axis=0)
+
+maxindices[maxindices == 0] = 10
+
+maxindices = np.expand_dims(maxindices, axis=1)
+
+print maxindices.shape
+print y.shape
+
+print y == maxindices
+print 100.0 * np.sum((y == maxindices) + 0) / 5000.0
+
+# logreg(
+#     X[random_indecies], (y[random_indecies] == 1) + 0, _lambda, 1.0, 1.0)
